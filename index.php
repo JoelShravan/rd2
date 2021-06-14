9f2367a5d738f341e17f0b8d6df471e979d9a57a
JoelShravan <80566214+JoelShravan@users.noreply.github.com>
2021-06-14 11:53:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 
+	<title>csv insert</title>
+</head>
+<body>
+   <form action="upload.php" method="POST" enctype="mulipart/form-data">
+   	<input type="file" name="op" />
+   	<button type="submit" value="submit">upload</button>
+ </form>     
+</body>
+</html>
